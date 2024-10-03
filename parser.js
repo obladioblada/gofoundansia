8d38f4d69963917c2cdc1066b5e03d78b806e5f8
obladioblada <gianpaolo.basilico@gmail.com>
2024-10-04 00:47:41 +0200

    add set variable

--- parser.js
+++ parser.js
@@ -9,19 +9,29 @@ app.use(express.json());
 const eventName = "stop-all-ansia-insieme"
 let url = 'https://www.gofundme.com/f/' + eventName;
 
-// Define a variable to change
-let variable = "0";
-
 // Serve static files from the 'public' folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+var otherSourceMoney = 0
 
 // API to get the current value of the variable
 app.get('/api/get-variable', async (req, res) => {
 
     // Repeat the task every 5 seconds (5000 milliseconds)
-    let collectedMoney = await fetchElement();
-    if (collectedMoney) res.json({ variable });
+    let variable = await fetchElement();
+    if (variable ) {
+        variable = Number(variable) + otherSourceMoney;
+        res.json({ variable })
+    };
     return res.status(500);
+});
+
+
+// API to update the variable
+app.post('/api/add-variable/:newValue', (req, res) => {
+    otherSourceMoney = otherSourceMoney + Number(req.params.newValue);
+    console.log(`Got ${otherSourceMoney} from other source! great!`);
+    res.json({ message: `Got ${otherSourceMoney} from other source! great!` });
 });
 
 // Start the server
